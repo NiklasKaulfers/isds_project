d51b956935a300f97dba034da353a8d79096d80d
Niklas Kaulfers <niklaskaulfers2@gmail.com>
2025-12-14 17:04:44 +0100

    works easier and better

--- src/heap-overflow.c
+++ src/heap-overflow.c
@@ -7,18 +7,11 @@ int main()
     // example for heap overflow
     char name[100]; 
     printf("Enter your name: ");
-    if (fgets(name, sizeof(name), stdin) != NULL) {
-        name[strcspn(name, "\n")] = '\0';
-
-        char *in = malloc(10);
-
-        strcpy(in, name);
-        printf("Hi: %s\n", in);
-
-        free(in);
-    } else {
-        fprintf(stderr, "Error reading input\n");
-    }
+    gets(name);
+    char *in = malloc(10);
+    strcpy(in, name);
+    printf("Hi: %s\n", in);
+    free(in);
 
     return 0;
 }
